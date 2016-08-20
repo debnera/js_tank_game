80d9b189e8408230932b052929021d868d53c50a
Anton Debner <anton.debner@gmail.com>
2016-08-20 12:40:29 +0300

    Add backbone for main(), init(), update(), draw()

---
+++ tank_game.js
@@ -0,0 +1,69 @@
+/*
+  Created by: Anton Debner 2016
+*/
+
+function main() {
+  /*
+    Creates a new HTML5 canvas element, adds listeners for input and
+    contains the main loop.
+  */
+  canvas = document.createElement("canvas")
+  canvas.width = WIDTH;
+  canvas.height = HEIGHT;
+  ctx = canvas.getContext("2d");
+
+  // Attempt to find a document element with specific id, otherwise attach the
+  // canvas to document body.
+  attach_to = document.getElementById('attached_script');
+  if (attach_to == null)
+  {
+      attach_to = document.body;
+  }
+  attach_to.appendChild(canvas);
+
+  // Add listeners for keydown and keyup
+  keystate = {};
+  document.addEventListener("keydown", function(evt) {
+    if (evt.keyCode === UpArrow || evt.keyCode === DownArrow) {
+      // Prevent up and down arrows from scrolling the website
+      evt.preventDefault();
+    }
+    keystate[evt.keyCode] = true;
+  });
+
+  document.addEventListener("keyup", function(evt) {
+    delete keystate[evt.keyCode];
+  });
+
+  init();
+  var loop = function() {
+    /*
+      The main loop where all the magic happens.
+    */
+    update();
+    draw();
+    window.requestAnimationFrame(loop, canvas);
+  };
+  window.requestAnimationFrame(loop, canvas);
+}
+
+function init() {
+  /*
+    Sets gameobjects to their starting values.
+  */
+  keystate = {}; // Reset the keystate to avoid stuck buttons
+}
+
+function update() {
+  /*
+    Handles game logic by moving all objects and checking collisions.
+  */
+}
+
+function draw() {
+  /*
+    Handles all drawing on the HTML5 canvas.
+  */
+}
+
+main();
