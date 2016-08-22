25c6e9a29b2de09a875d1d4450eefdca0165b83e
Anton Debner <anton.debner@gmail.com>
2016-08-22 12:43:26 +0300

    Implement random Kruskal maze generator

--- tank_game.js
+++ tank_game.js
@@ -2,21 +2,32 @@
   Created by: Anton Debner 2016
 */
 
-var WIDTH=1000, HEIGHT=600, pi=Math.PI;
+var WIDTH=900, HEIGHT=600;
 var UpArrow=38, DownArrow=40, LeftArrow=37, RightArrow=39;
 var canvas, ctx, keystate;
 var tank;
 var wall_width = 1;
-var cell_size = 30;
+var cell_size = 50;
+var num_cells_x, num_cells_y; // Assigned when cells are created
 var cells;
 
 class Cell {
 
-  constructor(x, y) {
+  constructor(x, y, i, j) {
+    this.ind_x = i;
+    this.ind_y = j;
     this.x = x;
     this.y = y;
     this.right_wall = true;
     this.bottom_wall = true;
+  }
+
+  remove_right_wall() {
+    this.right_wall = false;
+  }
+
+  remove_bottom_wall() {
+    this.bottom_wall = false;
   }
 
   draw() {
@@ -32,6 +43,116 @@ class Cell {
     }
   }
 };
+
+function get_cell_neighbours(x, y) {
+  var neighbours = [];
+  for (x2 = x - 1; x2 < x + 1; x2++) {
+    for (y2 = y - 1; y2 < y + 1; y2++) {
+      if (x2 >= 0 && x2 < num_cells_x && y2 >= 0 && y2 < num_cells_y) {
+        neighbours.push(cells[x2][y2]);
+      }
+    }
+  }
+  return neighbours;
+}
+
+function maze_generator_kruskal() {
+  /*
+    Uses randomized Kruskal's algorithm to generate a maze.
+    https://en.wikipedia.org/wiki/Maze_generation_algorithm#Randomized_Kruskal.27s_algorithm
+  */
+
+  function shuffle(a) {
+    /*
+      Shuffles array in place.
+      Taken from http://stackoverflow.com/questions/6274339/how-can-i-shuffle-an-array-in-javascript
+    */
+    var j, x, i;
+    for (i = a.length; i; i--) {
+      j = Math.floor(Math.random() * i);
+      x = a[i - 1];
+      a[i - 1] = a[j];
+      a[j] = x;
+    }
+  }
+
+  function find_cell_set(cell, sets) {
+    // Finds the set where the given cell is found.
+    for (set in sets) {
+      if (sets[set].has(cell)) return set;
+    }
+  }
+
+  function join_cell_sets(cell_1, cell_2, sets) {
+    /*
+      Checks if given cells are in different sets, joins the sets and returns
+      true. Otherwise returns false.
+    */
+    set_ind1 = find_cell_set(cell_1, sets);
+    set_ind2 = find_cell_set(cell_2, sets);
+    if (!(set_ind1 === set_ind2)) {
+      var joined_set = new Set(function*() {
+        yield* sets[set_ind1]; yield* sets[set_ind2]; }()
+      );
+      delete sets[set_ind1];
+      delete sets[set_ind2];
+      sets.push(joined_set);
+      return true;
+    }
+    return false;
+  }
+
+  // Add all walls to arrays and create a set for each cell
+  var right_walls = [];
+  var bottom_walls = [];
+  var cell_sets = [];
+  for (i = 0; i < num_cells_x; i++) {
+    for (j = 0; j < num_cells_y; j++) {
+      cell = cells[i][j];
+      cell_sets.push(new Set([cell]));
+      right_walls.push(cell);
+      bottom_walls.push(cell);
+    }
+  }
+
+  // Shuffle walls to randomize the maze
+  shuffle(right_walls);
+  shuffle(bottom_walls);
+
+  // These variables adjust the proportion of removed horizontal and vertical
+  // walls.
+  var horiz_prob = 0.6; // value must be 0 < x <= 1
+  var vert_prob = 0.9; // value must be 0 < x <= 1
+  var remove_anyway_prob = 0.4; // Probability for removing extra walls
+
+  // Remove all walls between disconnected cells
+  while (right_walls.length > 0 && bottom_walls.length > 0) {
+    if (right_walls.length > 0 && Math.random() < vert_prob) {
+      var cell = right_walls.pop();
+      if (cell.ind_x + 1 < num_cells_x) {
+        next_cell = cells[cell.ind_x+1][cell.ind_y];
+        if (join_cell_sets(cell, next_cell, cell_sets)) {
+          cell.right_wall = false;
+        }
+        // Randomly delete the wall anyway
+        else if (Math.random() < remove_anyway_prob) cell.right_wall = false;
+      }
+    }
+    if (bottom_walls.length > 0 && Math.random() < horiz_prob) {
+      var cell = bottom_walls.pop();
+      if (cell.ind_y + 1 < num_cells_y) {
+        next_cell = cells[cell.ind_x][cell.ind_y+1];
+        if (join_cell_sets(cell, next_cell, cell_sets)) {
+          cell.bottom_wall = false;
+        }
+        // Randomly delete the wall anyway
+        else if (Math.random() < remove_anyway_prob) cell.bottom_wall = false;
+      }
+    }
+  }
+
+
+}
 
 class Tank {
 
@@ -132,20 +253,20 @@ function init() {
   */
   keystate = {}; // Reset the keystate to avoid stuck buttons
   cells = [];
-  var num_width = Math.round(WIDTH / cell_size);
-  var num_height = Math.round(HEIGHT / cell_size);
-  for (i = 0; i < num_width; i++) {
+  num_cells_x = Math.round(WIDTH / cell_size);
+  num_cells_y = Math.round(HEIGHT / cell_size);
+  for (i = 0; i < num_cells_x; i++) {
     var x = i * cell_size + cell_size / 2;
     var column = []
 
-    for (j = 0; j < num_height; j++) {
+    for (j = 0; j < num_cells_y; j++) {
       var y = j * cell_size + cell_size / 2;
-      new_cell = new Cell(x, y);
+      new_cell = new Cell(x, y, i, j);
       column[j] = new_cell;
     }
     cells[i] = column;
   }
-
+  maze_generator_kruskal();
 }
 
 function update() {
@@ -173,10 +294,10 @@ function draw() {
   ctx.stroke();
 
   tank.draw();
-  var num_width = Math.round(WIDTH / cell_size);
-  var num_height = Math.round(HEIGHT / cell_size);
-  for (i = 0; i < num_width; i++) {
-    for (j = 0; j < num_height; j++) {
+  num_cells_x = Math.round(WIDTH / cell_size);
+  num_cells_y = Math.round(HEIGHT / cell_size);
+  for (i = 0; i < num_cells_x; i++) {
+    for (j = 0; j < num_cells_y; j++) {
       cells[i][j].draw();
     }
   }
